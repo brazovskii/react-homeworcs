828689c07f4cd1d2d78d70ba6e220d2eee2f4d00
Aleksei <aleksei.brazovskii@gmail.com>
2021-12-14 23:51:42 +0300

    add Timer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Dropdown from "./components/Dropdown";
+import Timer from "./components/Timer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dropdown />
+    <Timer />
   </React.StrictMode>,
   document.getElementById("root")
 );
